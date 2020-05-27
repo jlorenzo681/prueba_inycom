a32b3e1972a08f510a3e4084257e57c5f58f48f2
jlorenzop <jlorenzo681@gmail.com>
2020-05-27 12:47:11 +0200

    fix migration

--- src/Migrations/Version20200527104606.php
+++ src/Migrations/Version20200527104606.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200526143658 extends AbstractMigration
+final class Version20200527104606 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,12 +22,9 @@ final class Version20200526143658 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE charge_point ADD cpo_id INT DEFAULT NULL');
+        $this->addSql('CREATE TABLE charge_point (id INT AUTO_INCREMENT NOT NULL, cpo_id INT DEFAULT NULL, identity VARCHAR(255) NOT NULL, INDEX IDX_A181FF0C3C35C7EF (cpo_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE organization (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, legal_entity VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE charge_point ADD CONSTRAINT FK_A181FF0C3C35C7EF FOREIGN KEY (cpo_id) REFERENCES organization (id)');
-        $this->addSql('CREATE INDEX IDX_A181FF0C3C35C7EF ON charge_point (cpo_id)');
-        $this->addSql('ALTER TABLE organization DROP FOREIGN KEY FK_C1EE637C3C35C7EF');
-        $this->addSql('DROP INDEX IDX_C1EE637C3C35C7EF ON organization');
-        $this->addSql('ALTER TABLE organization DROP cpo_id');
     }
 
     public function down(Schema $schema) : void
@@ -36,10 +33,7 @@ final class Version20200526143658 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE charge_point DROP FOREIGN KEY FK_A181FF0C3C35C7EF');
-        $this->addSql('DROP INDEX IDX_A181FF0C3C35C7EF ON charge_point');
-        $this->addSql('ALTER TABLE charge_point DROP cpo_id');
-        $this->addSql('ALTER TABLE organization ADD cpo_id INT DEFAULT NULL');
-        $this->addSql('ALTER TABLE organization ADD CONSTRAINT FK_C1EE637C3C35C7EF FOREIGN KEY (cpo_id) REFERENCES charge_point (id)');
-        $this->addSql('CREATE INDEX IDX_C1EE637C3C35C7EF ON organization (cpo_id)');
+        $this->addSql('DROP TABLE charge_point');
+        $this->addSql('DROP TABLE organization');
     }
 }
